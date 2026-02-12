f8976714c9c6ea634418261a8f32ddd1e0d84837
asadkhan360 <asadmsharif4@gmail.com>
2026-02-12 22:51:39 +0500

    Fix DB host for Vercel (remove localhost)

--- config/database.js
+++ config/database.js
@@ -1,19 +1,19 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    port: 3306
+    port: 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-db.connect((err) => {
-    if (err) {
-        console.error("Error connecting to MySQL:", err);
-    } else {
-        console.log("Connected to MySQL");
-    }
-});
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASS:", process.env.DB_PASS ? "Loaded" : "Missing");
+console.log("DB_NAME:", process.env.DB_NAME);
 
 module.exports = db;
