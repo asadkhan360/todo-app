2370488102f7f81383291090be41b523f7db2ff7
asadkhan360 <asadmsharif4@gmail.com>
2026-02-13 17:32:44 +0500

    Initial commit - Todo App

--- api/todo.js
+++ api/todo.js
@@ -1,20 +1,48 @@
-const mysql = require('mysql2');
+const todoModel = require('../models/todo');
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-});
+export default function handler(req, res) {
+    const { method } = req;
 
-module.exports = (req, res) => {
-    if (req.method === 'GET') {
-        db.query('SELECT * FROM todos ORDER BY id DESC', (err, results) => {
-            if (err) {
-                console.log(err);
-                return res.status(500).json({ error: err });
-            }
-            res.status(200).json({ data: results });
+    if (method === 'GET') {
+        todoModel.getAllTodos((err, results) => {
+            if (err) return res.status(500).json({ error: err });
+            res.status(200).json({ data: results, total: results.length });
         });
     }
-};
+    else if (method === 'POST') {
+        const { title } = req.body;
+        if (!title) return res.status(400).json({ message: 'Title required' });
+
+        const wordCount = title.split(/\s+/).filter(w => w).length;
+        if (wordCount > 50)
+            return res.status(400).json({ message: 'Todo 50 words se zyada nahi ho sakta' });
+
+        todoModel.createTodo({ title }, (err, result) => {
+            if (err) return res.status(500).json({ error: err });
+            res.status(201).json({ message: 'Todo created', id: result.insertId });
+        });
+    }
+    else if (method === 'PUT') {
+        const id = req.query.id;
+        const { title } = req.body;
+        if (!id || !title) return res.status(400).json({ message: 'ID & Title required' });
+
+        todoModel.updateTodo(id, { title, updated_at: new Date() }, (err) => {
+            if (err) return res.status(500).json({ error: err });
+            res.status(200).json({ message: 'Todo updated' });
+        });
+    }
+    else if (method === 'DELETE') {
+        const id = req.query.id;
+        if (!id) return res.status(400).json({ message: 'ID required' });
+
+        todoModel.deleteTodo(id, (err) => {
+            if (err) return res.status(500).json({ error: err });
+            res.status(200).json({ message: 'Todo deleted' });
+        });
+    }
+    else {
+        res.setHeader('Allow', ['GET','POST','PUT','DELETE']);
+        res.status(405).end(`Method ${method} Not Allowed`);
+    }
+}
